6402520ed2268fdfeca5be2283c5f2c98083c92d
wuzhongyang <wuzhongyang@roobo.com>
2018-06-04 16:09:10 +0800

    修复delete的null与0

--- backend/models/cmsPresetAlbums.go
+++ backend/models/cmsPresetAlbums.go
@@ -1,9 +1,10 @@
 package models
 
 type CmsPresetAlbums struct {
-	GeneralWithDeleted `xorm:"extends"`
-	AppId              string `xorm:"varchar(255) notnull default('') 'appId'"`
-	AlbumId            int    `xorm:"BIGINT(20) notnull default(0)"`
+	General   `xorm:"extends"`
+	DeletedAt int    `xorm:"INT(11) deleted default(0) comment('软删除时间')"`
+	AppId     string `xorm:"varchar(255) notnull default('') 'appId'"`
+	AlbumId   int    `xorm:"BIGINT(20) notnull default(0)"`
 }
 
 func (c CmsPresetAlbums) TableName() string {
--- backend/models/upUpdate.go
+++ backend/models/upUpdate.go
@@ -1,26 +1,27 @@
 package models
 
 type UpUpdate struct {
-	GeneralWithDeleted `xorm:"extends"`
-	Production         string `xorm:"varchar(200) notnull default('pudding')"`
-	Local              string `xorm:"varchar(40) notnull default('zh_CN')"`
-	Channel            int    `xorm:"notnull default(10000)"`
-	Name               string `xorm:"varchar(100) notnull"`
-	Vcode              int    `xorm:"notnull default(0)"`
-	Vname              string `xorm:"varchar(40) notnull"`
-	Osvcode            int    `xorm:"notnull default(0)"`
-	Operation          string `xorm:"varchar(1024) notnull default('')"`
-	ExecuteTime        int    `xorm:"char(50) notnull default('now')"`
-	Minvcode           int    `xorm:"notnull default(0)"`
-	Maxvcode           int    `xorm:"notnull default(0)"`
-	Isforce            int    `xorm:"smallint(3) notnull default(0)"`
-	Filename           string `xorm:"varchar(100) notnull"`
-	Filesize           int    `xorm:"notnull"`
-	Dwnpath            string `xorm:"varchar(1024) notnull"`
-	DwnpathBk          string `xorm:"varchar(1024) notnull"`
-	Md5sum             int    `xorm:"char(32) notnull"`
-	Feature            string `xorm:"varchar(1024) default(NULL)"`
-	Ownname            string `xorm:"varchar(100) default(NULL)"`
+	General     `xorm:"extends"`
+	DeletedAt   int    `xorm:"INT(11) deleted comment('软删除时间')"`
+	Production  string `xorm:"varchar(200) notnull default('pudding')"`
+	Local       string `xorm:"varchar(40) notnull default('zh_CN')"`
+	Channel     int    `xorm:"notnull default(10000)"`
+	Name        string `xorm:"varchar(100) notnull"`
+	Vcode       int    `xorm:"notnull default(0)"`
+	Vname       string `xorm:"varchar(40) notnull"`
+	Osvcode     int    `xorm:"notnull default(0)"`
+	Operation   string `xorm:"varchar(1024) notnull default('')"`
+	ExecuteTime int    `xorm:"char(50) notnull default('now')"`
+	Minvcode    int    `xorm:"notnull default(0)"`
+	Maxvcode    int    `xorm:"notnull default(0)"`
+	Isforce     int    `xorm:"smallint(3) notnull default(0)"`
+	Filename    string `xorm:"varchar(100) notnull"`
+	Filesize    int    `xorm:"notnull"`
+	Dwnpath     string `xorm:"varchar(1024) notnull"`
+	DwnpathBk   string `xorm:"varchar(1024) notnull"`
+	Md5sum      int    `xorm:"char(32) notnull"`
+	Feature     string `xorm:"varchar(1024) default(NULL)"`
+	Ownname     string `xorm:"varchar(100) default(NULL)"`
 }
 
 func (c UpUpdate) TableName() string {
